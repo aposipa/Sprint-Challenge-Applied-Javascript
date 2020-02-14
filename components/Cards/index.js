8bd0d93032ac8377f14ba18ab31e44aead6f6973
aposipa <andrew.posipanka@gmail.com>
2020-02-14 13:28:21 -0500

    finished the last task of completing the cards on the page with the data from axios.get.

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,65 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(response => {
+        console.log(response.data.articles);
+        response.data.articles.javascript.forEach(item =>{
+            cardsContainer.append(Articles(item))
+            
+})
+
+response.data.articles.bootstrap.forEach(item =>{
+    cardsContainer.append(Articles(item))
+    
+})
+response.data.articles.technology.forEach(item =>{
+    cardsContainer.append(Articles(item))
+    
+})
+
+response.data.articles.jquery.forEach(item =>{
+    cardsContainer.append(Articles(item))
+    
+})
+response.data.articles.node.forEach(item =>{
+    cardsContainer.append(Articles(item))
+    
+})
+
+})
+
+
+function Articles(object) {
+
+const card = document.createElement('div');
+const headline = document.createElement('div');
+const authorTitle = document.createElement('div');
+const imgContainer = document.createElement('div');
+const img = document.createElement('img');
+const authorName = document.createElement('span');
+//end create element
+
+card.append(headline, authorTitle);
+authorTitle.append(imgContainer, authorName);
+imgContainer.append(img);
+//end appending children
+
+card.classList.add('card');
+headline.classList.add('headline');
+authorTitle.classList.add('author');
+imgContainer.classList.add('img-container');
+//end create class
+
+headline.textContent = 'Headline: ' + object.headline;
+img.src = object.authorPhoto;
+authorName.textContent = 'By: ' + object.authorName;
+
+return card;
+}
+const cardsContainer = document.querySelector('.cards-container');
+// cardsContainer.append(Articles());
+
+
+
