1bab6600e438f200b8dd29f1251562fd83436f55
aposipa <andrew.posipanka@gmail.com>
2020-02-14 11:40:59 -0500

    Finished the README questions and the first header section.

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,32 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header() {
+
+const header = document.createElement('div');
+const date = document.createElement('span');
+const title = document.createElement('h1');
+const temp = document.createElement('span');
+//end created elements
+
+date.textContent = "MARCH 28, 2019";
+title.textContent = "Lambda Times";
+temp.textContent = '98°';
+//end textContent
+
+header.classList.add('header');
+date.classList.add('span');
+temp.classList.add('temp');
+//end class lists
+
+
+
+header.append(date, title, temp);
+headerContainer.append(header);
+//end append children
+
+return header;
+
+}
+const headerContainer = document.querySelector('.header-container');
+headerContainer.append(Header());
